456d793b8c47fda8cc26438ff55aed259c02dd9b
Hammerlord <nepharus@gmail.com>
2022-01-30 11:52:26 -0800

    Add descriptions to setting options

--- src/SettingsDialog.tsx
+++ src/SettingsDialog.tsx
@@ -1,11 +1,25 @@
 import { Checkbox, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
 import { useState } from "react";
+import { createUseStyles } from "react-jss";
 import Button from "./Button";
 
 export interface GameSettings {
     isMapleStoryDictionEnabled: boolean;
     isHardMode: boolean;
 }
+
+const useStyles = createUseStyles({
+    option: {
+        display: "flex",
+        marginBottom: "1.5rem",
+    },
+    optionTitle: {
+        fontWeight: 700,
+    },
+    checkboxContainer: {
+        marginTop: "-8px",
+    },
+});
 
 const SettingsDialog = ({ settings, onApplySettings, onClose }) => {
     const [workingSettings, setWorkingSettings] = useState(settings);
@@ -24,22 +38,34 @@ const SettingsDialog = ({ settings, onApplySettings, onClose }) => {
         }));
     };
 
+    const classes = useStyles();
     return (
         <Dialog open={true} onClose={onClose} disablePortal={true}>
             <DialogTitle>Settings</DialogTitle>
             <DialogContent>
                 <div>
-                    <label>
-                        <Checkbox onClick={onToggleMapleStoryDiction} checked={workingSettings.isMapleStoryDictionEnabled} />
-                        Enable MapleStory diction
+                    <label className={classes.option}>
+                        <div className={classes.checkboxContainer}>
+                            <Checkbox onClick={onToggleMapleStoryDiction} checked={workingSettings.isMapleStoryDictionEnabled} />
+                        </div>
+                        <div>
+                            <div className={classes.optionTitle}>MapleStory diction</div>
+                            Correct answers may include MapleStory terminology.
+                        </div>
                     </label>
                 </div>
                 <div>
-                    <label>
-                        <Checkbox onClick={onToggleHardMode} checked={workingSettings.isHardMode} />
-                        Enable hard mode
+                    <label className={classes.option}>
+                        <div className={classes.checkboxContainer}>
+                            <Checkbox onClick={onToggleHardMode} checked={workingSettings.isHardMode} />
+                        </div>
+                        <div>
+                            <div className={classes.optionTitle}>Hard mode</div>
+                            Guesses must use the green and yellow hints that are revealed.
+                        </div>
                     </label>
                 </div>
+                <hr />
                 <p>*Applying changes will clear your stats and restart the game.</p>
             </DialogContent>
             <DialogActions>
